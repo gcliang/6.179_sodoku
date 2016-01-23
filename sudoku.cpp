fd08cea8f7e4c219b4c737ccd650416138a4e43a
George Liang <gcliang@mit.edu>
2016-01-23 17:37:35 -0500

    Laid framework for initializing board and reading in input

--- sudoku.cpp
+++ sudoku.cpp
@@ -1,9 +1,15 @@
 #include <iostream>
 #include <set>
+#include <cstdlib>
+#include <sstream>
 #include "sudoku.h"
 
 using namespace std;
 const int N = 9;
+const int EASY = 5;
+const int MEDIUM = 6;
+const int HARD = 7;
+const int COMPLETED_BOARDS = 3;
 
 class OutOfBoundsException : public exception {
 	virtual const char* what() const throw() {
@@ -60,9 +66,9 @@ class Row {
 		set<int> values;
 	
 	public:
-		Unit *getUnits() {
-			return units;
-		}
+//		Unit *getUnits() {
+//			return units;
+//		}
 
 		void insert(int position, int value) {
 			if (units[position] != NULL) {
@@ -95,9 +101,9 @@ class Column {
 		set<int> values;
 	
 	public:
-		Unit *getUnits() {
-			return units;
-		}
+//		Unit *getUnits() {
+//			return units;
+//		}
 
 		void insert(int position, int value) {
 			if (units[position] != NULL) {
@@ -130,9 +136,9 @@ class Section {
 		set<int> values;
 	
 	public:
-		Unit *getUnits() {
-			return units;
-		}
+//		Unit *getUnits() {
+//			return units;
+//		}
 
 		void insert(int position, int value) {
 			if (units[position] != NULL) {
@@ -164,5 +170,68 @@ class Sudoku {
 };
 
 int main() {
-  return 0;
+	// Determine desired difficulty
+	cout << "Welcome to Sudoku.  What is your desired difficulty? \n" << "The options are easy, medium, and hard.\n";
+	string difficulty;
+	int diff;
+	bool done = false;
+	getline(cin, difficulty);
+
+	while (difficulty != "easy" && difficulty != "medium" && difficulty != "hard") {
+		cout << "Invalid difficulty, please try again.\n";
+		getline(cin, difficulty);
+	}
+
+	if (difficulty == "easy") {
+		diff = EASY;
+	} else if (difficulty == "medium") {
+		diff = MEDIUM;
+	} else if (difficulty == "hard") {
+		diff = HARD;
+	}
+
+	cout << "You have chosen " << difficulty << ".\nBuilding your puzzle...\n";
+
+	// Chose inital sudoku board from selected completed board
+	int board1[N][N] = {{3,9,4,1,7,2,5,8,6},
+						{1,5,7,3,8,6,2,4,9},
+						{2,8,6,9,4,5,7,1,3},
+						{5,3,8,7,9,4,6,2,1},
+						{9,4,1,2,6,3,8,7,5},
+						{7,6,2,8,5,1,3,9,4},
+						{4,1,3,5,2,8,9,6,7},
+						{6,2,9,4,3,7,1,5,8},
+						{8,7,5,6,1,9,4,3,2}
+						};
+	int board2[N][N];
+	int board3[N][N];
+
+	int (*possibleBoards[COMPLETED_BOARDS])[N][N] = {&board1, &board2, &board3};
+
+	int randomIndex = rand() % COMPLETED_BOARDS;
+	int (*chosen)[9][9] = (possibleBoards[randomIndex]);
+
+	Sudoku mainBoard;
+	// randomly remove numbers to reach initial state
+
+	cout << "Fill in the 0's with the correct number (1-9) by inputing:\nx-coordinate y-coordinate digit";
+
+	// loop to get inputs and update map
+	while (!done) {
+		// print out board
+
+		// wait for output
+		string input;
+		getline(cin, input);
+
+		stringstream ss(input);
+		int x, y, digit;
+		ss >> x;
+		ss >> y;
+		ss >> digit;
+
+		// print out board with possible update
+	}
+
+	return 0;
 }
