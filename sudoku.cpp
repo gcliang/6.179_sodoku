5b85c896290be0c668d7d6da28d972259cf33121
Kayode Ezike <kezike17@mit.edu>
2016-01-24 17:11:56 -0500

    Implemented insert() and isComplete() methods on Sudoku

--- sudoku.cpp
+++ sudoku.cpp
@@ -16,13 +16,13 @@ const int COMPLETED_BOARDS = 3;
 
 class OutOfBoundsException : public exception {
 	virtual const char* what() const throw() {
-		return "That position is out of bounds!";
+		return "That position is out of bounds! Please try again.";
 	}
 } outOfBoundsException;
 
 class SudokuInvariantException : public exception {
 	virtual const char* what() const throw() {
-		return "That move violates the Sudoku invariant. Try again!";
+		return "That move violates the Sudoku invariant! Please try again.";
 	}
 } sudokuInvariantException;
 
@@ -294,7 +294,7 @@ class Sudoku {
 			} else if (!isValid(x, y, value)) {
 				throw sudokuInvariantException;
 			}
-			// TODO: INSERT INTO APPROPRIATE ROW, COLUMN, AND SECTION
+			board[y * N + x]->setValue(value);
 		}
 
 		bool isValid(int x, int y, int value) {
@@ -308,6 +308,15 @@ class Sudoku {
 		}
 
 		bool isComplete() {
+			int i;
+			for (i = 0; i < N; i++) {
+				int j;
+				for (j = 0; j < N; j++) {
+					if (board[i * N + j]->getValue() == 0) {
+						return false;
+					}
+				}
+			}
 			return true;
 		}
 };
@@ -396,22 +405,27 @@ int main() {
 
 	Sudoku *mainBoard = new Sudoku(chosen);
 
+	cout << "Sudoku constructed\n";
+
 	// randomly remove numbers to reach initial state
 	int row, d;
 	Row *current;
 	for (row = 0; row < N; row++) {
+		cout << "Random outer for loop" << '\n';
 		current = mainBoard->getRows()[row];
 		int removed[diff];
 		int col;
 		for (d = 0; d < diff; d++) {
+			cout << "Random inner for loop" << '\n';
 			col = rand() % N;
 			int * found;
 			found = find(removed, removed+diff, col);
 			while (found == removed+diff) {
+				cout << "Random while loop" << '\n';
 				col = rand() % N;
 				found = find(removed, removed+diff, col);
 			}
-			current->insert(0, current->getUnits()[col]);
+			current->getUnits()[col]->setValue(0);
 		}
 	}
 	
