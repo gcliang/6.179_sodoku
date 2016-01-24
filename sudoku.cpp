bda2466dbff0fd41b4052e868418b749584d6e2c
Kayode Ezike <kezike17@mit.edu>
2016-01-24 17:38:04 -0500

    Implemented isValid() on Sudoku...not entirely correct yet

--- sudoku.cpp
+++ sudoku.cpp
@@ -299,12 +299,10 @@ class Sudoku {
 		}
 
 		bool isValid(int x, int y, int value) {
-			// TODO: check appropriate row
-			bool isValidRow;
-			// TODO: check appropriate column
-			bool isValidColumn;
+			bool isValidRow = getRows()[y]->isValid(x, value);
+			bool isValidColumn = getColumns()[x]->isValid(y, value);
 			// TODO: check appropriate section
-			bool isValidSection;
+			bool isValidSection = true;
 			return isValidRow && isValidColumn && isValidSection;
 		}
 
